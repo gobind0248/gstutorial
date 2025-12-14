ea6cf45053585fbbebeddc5d53a8eb079efbc70b
gobind0248 <gobind.bngn@gmail.com>
2025-12-14 15:41:32 +0530

    Update and rename sheets-config.js to config.js

--- sheets-config.js
+++
@@ -1,5 +0,0 @@
-const GOOGLE_SHEETS_CONFIG = {
-  sheetId: "1G6tTHT56T6FmqOwOvVFVPoVqoSs-TSBz_-SiW9imQvk",
-  gid: "0", // Sheet ID (gid parameter)
-  jsonExportUrl: "https://docs.google.com/spreadsheets/d/1G6tTHT56T6FmqOwOvVFVPoVqoSs-TSBz_-SiW9imQvk/gviz/tq?tqx=out:json&gid=0"
-};
