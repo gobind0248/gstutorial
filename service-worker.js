900009b94cf6ddeb469cde4c5de2a45cdf45247a
gobind0248 <gobind.bngn@gmail.com>
2025-12-17 19:02:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gst-mcq-v2.1"; // 🔴 CHANGE VERSION TO FORCE UPDATE
+const CACHE_NAME = "gst-mcq-v3"; // 🔴 CHANGE VERSION TO FORCE UPDATE
 
 self.addEventListener("install", event => {
   self.skipWaiting();
